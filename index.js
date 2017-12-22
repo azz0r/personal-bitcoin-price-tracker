af710323fffe237a885a56df8a21b20e9096be99
azz0r <azz0r@azz0rs-MacBook-Pro.local>
2017-12-22 14:50:47 +0000

    Stripping the boilerplate away

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import logo from './logo.svg';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-const App = () => (
-  <div className="App">
-    <img className="App-Logo" src={logo} alt="React Logo" />
-    <h1 className="App-Title">Hello Parcel x React</h1>
-  </div>
-);
+const App = () => <div className="app">Hello there</div>;
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // Hot Module Replacement
 if (module.hot) {
